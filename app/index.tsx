2186868f24fc95b06e92d023e60554c7e122e45e
Sa'ad <spatel@horizontal.com>
2025-08-29 16:45:46 +0530

    init

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { ScreenContent } from 'components/ScreenContent';
+import { View, Text } from 'react-native';
+
+const Home = () => {
+  return (
+    <>
+      <Text className="text-blue-400">Sample</Text>
+    </>
+  );
+};
+
+export default Home;
--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,7 +1,7 @@
 import { Text, View } from 'react-native';
 
 export const EditScreenInfo = ({ path }: { path: string }) => {
-  const title = 'Open up the code for this screen:';
+  const title = 'Yo!! Open up the code for this screen:';
   const description =
     'Change any of the text, save the file, and your app will automatically update.';
 
@@ -21,7 +21,7 @@ export const EditScreenInfo = ({ path }: { path: string }) => {
 const styles = {
   codeHighlightContainer: `rounded-md px-1`,
   getStartedContainer: `items-center mx-12`,
-  getStartedText: `text-lg leading-6 text-center`,
+  getStartedText: `text-lg leading-6 text-center color-orange`,
   helpContainer: `items-center mx-5 mt-4`,
   helpLink: `py-4`,
   helpLinkText: `text-center`,
