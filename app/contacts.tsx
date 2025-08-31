15d0b1275cce769f13e1aff074d796b1c17af809
Sa'ad <patelsaad39@gmail.com>
2025-08-31 22:34:13 +0530

    update

--- app/contacts.tsx
+++ app/contacts.tsx
@@ -1,7 +1,24 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
+import * as ContactsModule from "expo-contacts";
 
 const Contacts = () => {
+  useEffect(() => {
+    (async () => {
+      const { status } = await ContactsModule.requestPermissionsAsync();
+      if (status === "granted") {
+        const { data } = await ContactsModule.getContactsAsync({
+          fields: [ContactsModule.Fields.Emails],
+        });
+
+        if (data.length > 0) {
+          const contact = data[0];
+          console.log(contact);
+        }
+      }
+    })();
+  }, []);
+
   return (
     <View>
       <Text>Contacts</Text>
